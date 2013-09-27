f0a22b185b8041fffd1268364ab5f19080078521
Konstantin Chaplaev <chaplaev@gmail.com>
2013-09-27 21:10:07 +0400

    Add simulation code for RRD 2.4 (Win32, gcc version 4.8.1 (rev4, Built by MinGW-builds project)).

---
+++ rrd/dclp.cpp
@@ -0,0 +1,18 @@
+
+#include "stdafx.h"
+#include <relacy/relacy_std.hpp>
+
+#include "dclp.h"
+
+
+int main()
+{
+	rl::test_params p;
+
+	p.iteration_count = 1000000;
+	p.search_type = rl::sched_bound;
+	p.context_bound = 5;
+	rl::simulate<dlcp_test>(p);
+
+	return 0;
+}
---
+++ rrd/dclp.h
@@ -0,0 +1,88 @@
+
+#include "stdafx.h"
+#include <relacy/relacy_std.hpp>
+
+
+const uint32_t Sign0 = 1122334455;
+
+
+// This class is huge, so we use DCLP approach.
+class huge_class
+{
+public:
+	std::atomic<uint32_t> sign0;
+
+	huge_class() { sign0.store(Sign0, std::memory_order_release); }
+	uint32_t get(){ return sign0.load(std::memory_order_acquire); }
+};
+
+template <typename T>
+class dlcp
+{
+private:
+	std::atomic<T *> pInstance;
+	std::mutex guard;
+
+public:
+
+	dlcp() : pInstance(nullptr) {}
+
+	T *get_instance()
+	{
+		T *tmp = pInstance.load(std::memory_order_relaxed);
+		// test mb
+		//atomic_thread_fence(std::memory_order_acquire);
+		if (tmp == nullptr) {
+			guard.lock($);
+			tmp = pInstance.load(std::memory_order_acquire);
+			if (tmp == nullptr) {
+				tmp = new T();
+				// test mb
+				//atomic_thread_fence(std::memory_order_release);
+				pInstance.store(tmp, std::memory_order_relaxed);
+			}
+			guard.unlock($);
+		}
+		return tmp;
+	}
+
+	void put_instance()
+	{
+		guard.lock($);
+		T *tmp = pInstance.load(std::memory_order_acquire);
+		if (tmp) {
+			pInstance.store(nullptr, std::memory_order_release);
+			atomic_thread_fence(std::memory_order_seq_cst);
+			delete tmp;
+		}
+		guard.unlock($);
+	}
+};
+
+struct dlcp_test : rl::test_suite<dlcp_test, 2> {
+	dlcp<huge_class> t;
+	uint32_t val1, val2;
+	huge_class *p1, *p2;
+
+	void before() { t.put_instance(); }
+
+	void thread(unsigned thread_index)
+	{
+		if ((thread_index % 2) == 0) {
+			p1 = t.get_instance();
+			val1 = p1->get();
+			RL_ASSERT(val1 == Sign0);
+
+		} else if ((thread_index % 2) != 0) {
+			p2 = t.get_instance();
+			val2 = p2->get();
+			RL_ASSERT(val2 == Sign0);
+		}
+	}
+
+	void after()
+	{
+		RL_ASSERT(p1 == p2);
+		t.put_instance();
+	}
+};
